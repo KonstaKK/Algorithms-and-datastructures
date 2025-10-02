b2e291e11f7a28f6c60ed5840de144da491a2e40
Konsta Karttunen <ekoka001@edu.xamk.fi>
2025-10-02 09:32:41 +0300

    corrections to the homework

--- Analysis/analysis.cpp
+++ Analysis/analysis.cpp
@@ -22,9 +22,9 @@ void simpleSort(float* a, int n) {
     std::cout << "Sorting took ~" << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() << " nanoseconds" << std::endl;
     // 300000 values is already a stack overflow
     // Sorting took ~37200 nanoseconds for 100 values
-    // Sorting took ~1986900 nanoseconds for 1000 values
-    // Sorting took ~144075300 nanoseconds for 10000 values
-    // Sorting took ~12979807300 nanoseconds for 100000 values
+    // Sorting took ~1986900 nanoseconds for 1000 values -> 0.002 seconds
+    // Sorting took ~144075300 nanoseconds for 10000 values -> 0.14 seconds
+    // Sorting took ~12979807300 nanoseconds for 100000 values -> 12.3 seconds
 
     // The function does not follow the O(f(n)) that was found
     // Swapping values between each other takes its own time
