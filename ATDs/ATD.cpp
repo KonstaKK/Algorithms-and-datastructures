90deb38f4ca21d8bc1ad34c99ed86c32387811c5
Konsta Karttunen <ekoka001@edu.xamk.fi>
2025-10-02 11:20:08 +0300

    new list functions

--- ATDs/ATD.cpp
+++ ATDs/ATD.cpp
@@ -1,3 +1,16 @@
-int main() {
+#include "LinkedList.h"
+#include "Node.h"
+#include <iostream>
+#include <iomanip>
 
+int main() {
+    LinkedList list;
+
+    const int size = 10;
+
+    for (int i = 0; i < size; i++) {
+        list.Add(rand() % (size + 1));
+    }
+
+    list.Print();
 }
--- ATDs/LinkedList.cpp
+++ ATDs/LinkedList.cpp
@@ -1,7 +1,30 @@
 #include "LinkedList.h"
+#include <iostream>
 
 bool LinkedList::Add(int data) {
-	// Tyhjä lista = luodaan node ja laitetaan
+	// Tyhjä lista = luodaan node ja laitetaan headi osoittamaan uuteen nodeen
+
+	if (nullptr == pHead) {
+		Node* pNewNode = new Node(data);	// luotiin uusi node arvolla "val"
+		pHead = pNewNode;					// head-pointer osoittaa uuteen nodeen
+		return true;
+	} // Lisää aina alkuun ( 0(1)-aikainen
+	else {
+		Node* pNewNode = new Node(data);	// luotiin uusi node arvolla "val"
+		pNewNode->Next = pHead;				// asetettiin uuden noden next-nodeksi vanha head
+		pHead = pNewNode;					// head-pointer osoittaa uuteen nodeen
+		return true;
+	}
 
 	return false;
+}
+
+// Tulostaa listan sisällön kokonaan
+void LinkedList::Print() {
+	Node* pCurrent = pHead;
+	
+	while (nullptr != pCurrent) {
+		std::cout << pCurrent->Data << std::endl;
+		pCurrent = pCurrent->Next;
+	}
 }
