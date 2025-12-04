609bbe8a737f817ce87ace76d63e853035d9ebf9
Konsta Karttunen <ekoka001@edu.xamk.fi>
2025-12-04 12:15:05 +0200

    Started the pathfinding

---
+++ Pathfinder/Assets/Scriptis/Pathfinder.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    public GameObject Tile;
+
+    public int Width = 20;
+    public int Height = 20;
+    public float Spacing = 2.05f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int y = 0; y < Height; y++)
+        {
+            for (int x = 0; x < Width; x++)
+            {
+                Instantiate(Tile, new Vector3(x * Spacing - Width / 2 * Spacing, y * Spacing - Height / 2 * Spacing, 0), Tile.transform.rotation);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
