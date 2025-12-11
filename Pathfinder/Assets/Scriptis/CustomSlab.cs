e52f1f3e64977801da65e732673e8d5ffee02d50
Konsta Karttunen <ekoka001@edu.xamk.fi>
2025-12-11 12:04:31 +0200

    Pathfinder complete, mostly

--- Pathfinder/Assets/Scriptis/CustomSlab.cs
+++ Pathfinder/Assets/Scriptis/CustomSlab.cs
@@ -1,42 +1,86 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
+// Tile for Dijkstra's algorithm
 public class CustomSlab : MonoBehaviour
 {
-    private int cost = 0;
-    public int x = -1;
-    public int y = -1;
+    // Smallest cost from start to this tile
+    [SerializeField]
+    private int cost = -1;
+
+    public bool IsWalkable = true;
+
+    // Coordinates for this tile in the grid
+    public Vector2Int Pos = new Vector2Int(-1, -1);
+
+    // Where did it come from
+    public Vector2Int Origin = new Vector2Int(-1, -1);
+
+    // Just for accessing the text component
+    private TextMeshPro myTMP;
 
     public bool isDirty = true;
 
-    private TextMeshProUGUI myTMP;
-
-
-    public void setCoords(int _x, int _y)
-    {
-        x = _x;
-        y = _y;
-    }
-    public void setCost(int _cost)
-    {
-        this.cost = _cost;
-    }
-
+    // For accessing and modifying material color
+    private Material myMaterial;
 
     // Start is called before the first frame update
     void Start()
     {
-        myTMP = gameObject.GetComponentInChildren<TextMeshProUGUI>();
+        // Populate myTMP at the Start
+        myTMP = gameObject.GetComponentInChildren<TextMeshPro>();
+        // Populate myMaterial at the Start
+        myMaterial = GetComponent<Renderer>().material;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (myTMP != null)
-            myTMP.SetText(cost.ToString());
+        if (myTMP != null && isDirty)
+        {
+            if (cost == int.MaxValue)
+            {
+                myTMP.text = "MAX";
+            }
+            else
+            {
+                myTMP.SetText(cost.ToString());
+            }
+
+            if (!IsWalkable)
+            {
+                myTMP.text = "XXX";
+                SetColor(Color.black);
+            }
+            isDirty = false;
+        }
+        else if (myTMP == null)
+        {
+            Debug.Log("Text Mesh Pro component not found!: " + myTMP);
+        }
+    }
+
+    // Cost must be updated using this function
+    public void SetCost(int _cost)
+    {
+        cost = _cost;
+        isDirty = true;
+    }
+
+    public void SetColor(Color _color)
+    {
+        if (myMaterial != null)
+        {
+            myMaterial.color = _color;
+        }
         else
-            Debug.Log("Text Mesh Pro component not found!");
+        {
+            Debug.Log($"Material not found: {myMaterial == null}");
+        }
+    }
+
+    public int GetCost()
+    {
+        return cost;
     }
 }
--- Pathfinder/Assets/Scriptis/Pathfinder.cs
+++ Pathfinder/Assets/Scriptis/Pathfinder.cs
@@ -1,5 +1,5 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Pathfinder : MonoBehaviour
@@ -8,7 +8,21 @@ public class Pathfinder : MonoBehaviour
 
     public int Width = 20;
     public int Height = 20;
-    public float Spacing = 2.05f;
+    public float Spacing = 1.1f;
+
+    public Vector2Int StartPos = new Vector2Int(1, 2);
+
+    public Vector2Int TargetPos = new Vector2Int(19, 17);
+
+    public List<Vector2Int> ObstaclePos = new List<Vector2Int>();
+
+    // Unvisited list of slabs
+    private List<CustomSlab> unvisitedSlabs = new List<CustomSlab>();
+
+    // Visited list of slabs
+    private List<CustomSlab> visitedSlabs = new List<CustomSlab>();
+
+    private bool bStart = true;
 
     // Start is called before the first frame update
     void Start()
@@ -17,23 +31,129 @@ public class Pathfinder : MonoBehaviour
         {
             for (int x = 0; x < Width; x++)
             {
-                Instantiate(Tile, new Vector3(x * Spacing - Width / 2 * Spacing, y * Spacing - Height / 2 * Spacing, 0), Tile.transform.rotation);
+                GameObject obj = Instantiate(Tile, new Vector3(x * Spacing, y * Spacing, 0), Tile.transform.rotation);
+                CustomSlab slab = obj.GetComponent<CustomSlab>();
+                slab.SetCost(int.MaxValue);
+                slab.Pos = new Vector2Int(x + 1, y + 1);
+
+                foreach (Vector2Int pos in ObstaclePos)
+                {
+                    if (slab.Pos == pos)
+                    {
+                        slab.IsWalkable = false;
+                        break;
+                    }
+                }
+
+                if (slab.Pos == StartPos)
+                {
+                    slab.SetCost(0);
+                }
+
+                if (slab.IsWalkable)
+                {
+                    unvisitedSlabs.Add(slab);
+                }
             }
         }
 
-        if (Height < Width)
-        {
-            Camera.main.transform.position = new Vector3(0, 0, -Width * Spacing);
-        }
-        else
-        {
-            Camera.main.transform.position = new Vector3(0, 0, -Height * Spacing);
-        }
+        Camera.main.transform.position = new Vector3(Width / 2 * Spacing, Height / 2 * Spacing, (Height < Width) ? -Width * Spacing : -Height * Spacing);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (bStart)
+        {
+            // Set colors and costs
+            foreach (CustomSlab slab in unvisitedSlabs)
+            {
+                if (slab.Pos == StartPos)
+                {
+                    slab.SetColor(Color.green);
+                }
+                else if (slab.Pos == TargetPos)
+                {
+                    slab.SetColor(Color.red);
+                }
+            }
+            Dijkstra();
+            TraceRoute();
+            bStart = false;
+        }
+    }
+
+    private void Dijkstra()
+    {
+        // While unvisited list is not empty
+        // -Select the tile with the lowest cost
+        // -Remove it from the list and set it as current tile
+        // -Go through its surrounding slabs (Ignore the visited ones)
+        // -Add the current tile to the visited list
+
+        while (unvisitedSlabs.Count > 0)
+        {
+            int minCost = unvisitedSlabs.Min(slab => slab.GetCost());
+            CustomSlab currentTile = unvisitedSlabs.Find(slab => slab.GetCost() == minCost);
+            //CustomSlab currentTile = unvisitedSlabs.Find(s => s.Pos == StartPos);
+            unvisitedSlabs.Remove(currentTile);
+
+            for (int x = currentTile.Pos.x - 1; x <= currentTile.Pos.x + 1; x++)
+            {
+                for (int y = currentTile.Pos.y - 1; y <= currentTile.Pos.y + 1; y++)
+                {
+                    if (x < 1 || x > Width || y < 1 || y > Height)
+                    {
+                        continue;
+                    }
+
+                    Vector2Int neighborPos = new Vector2Int(x, y);
+                    if (neighborPos == currentTile.Pos)
+                    {
+                        continue;
+                    }
+
+                    CustomSlab neighbor = unvisitedSlabs.Find(tile => tile.Pos == neighborPos);
+                    if (neighbor == null)
+                    {
+                        continue;
+                    }
+
+                    Vector2Int delta = neighborPos - currentTile.Pos;
+                    delta.x = Mathf.Abs(delta.x);
+                    delta.y = Mathf.Abs(delta.y);
+                    int moveCost = (delta.x + delta.y == 1) ? 10 : 14;
+
+                    int costToNeighbor = currentTile.GetCost() + moveCost;
+
+                    if (costToNeighbor < neighbor.GetCost())
+                    {
+                        neighbor.SetCost(costToNeighbor);
+                        neighbor.Origin = currentTile.Pos;
+                    }
+                }
+            }
+
+            visitedSlabs.Add(currentTile);
+        }
+    }
+
+    private void TraceRoute()
+    {
+        CustomSlab slab = visitedSlabs.Find(s => s.Pos == TargetPos);
+        if (slab == null)
+        {
+            return;
+        }
+
+        while (slab.Pos != StartPos)
+        {
+            slab = visitedSlabs.Find(s => s.Pos == slab.Origin);
+            if (slab.Pos == StartPos)
+            {
+                break;
+            }
+            slab.SetColor(Color.blue);
+        }
     }
 }
